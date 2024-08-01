6bd81305967468ba8d4d6b371d55f648b3d16e47
Elyor Tukhtamuratov <tuxtamuratovelyor@gmail.com>
2024-08-01 13:42:44 +0500

    fix

--- utils/username.py
+++ utils/username.py
@@ -3,7 +3,7 @@ import re
 
 def extract_username(message_text):
     username_match = re.search(r"@\w+", message_text)
-    username = username_match.group(0)[1:] if username_match else None
+    username = username_match.group(0) if username_match else None
 
     user_id_match = re.search(r"\b\d+\b", message_text)
     user_id = int(user_id_match.group(0)) if user_id_match else None
