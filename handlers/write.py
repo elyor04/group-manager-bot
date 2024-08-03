d763c086f1eb940a6862ae3bef35d633e2eb4a6c
Elyor Tukhtamuratov <tuxtamuratovelyor@gmail.com>
2024-08-04 02:13:09 +0500

    update

--- handlers/write.py
+++ handlers/write.py
@@ -4,13 +4,12 @@ from aiogram import types
 allowed_users = [7084938423, 1398600688]
 
 
-async def write_by_user(message: types.Message):
+async def write_by_bot(message: types.Message):
     if (message.from_user.id not in allowed_users) or (not message.get_args()):
         return
 
     if message.reply_to_message:
         message_sender = message.reply_to_message.reply
-
     else:
         message_sender = message.answer
 
@@ -20,7 +19,7 @@ async def write_by_user(message: types.Message):
 
 def register_write_handlers(dp: Dispatcher):
     dp.register_message_handler(
-        write_by_user,
+        write_by_bot,
         commands=["write"],
         chat_type=[types.ChatType.GROUP, types.ChatType.SUPERGROUP],
     )
