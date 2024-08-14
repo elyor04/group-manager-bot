87ee12a74a8e2e40bf8891780a3ceb04053d4399
Elyor Tukhtamuratov <tuxtamuratovelyor@gmail.com>
2024-08-14 09:29:19 +0500

    change

--- handlers/member.py
+++ handlers/member.py
@@ -4,14 +4,14 @@ from aiogram import Dispatcher, types
 async def on_member_joined(message: types.Message):
     for member in message.new_chat_members:
         await message.answer(
-            f'Welcome to the group, <a href="tg://user?id={member.id}">{member.full_name}</a>!'
+            f'Welcome to the group, <a href="tg://user?id={member.id}">{member.full_name}</a>'
         )
 
 
 async def on_member_left(message: types.Message):
     member = message.left_chat_member
     await message.answer(
-        f'Goodbye, <a href="tg://user?id={member.id}">{member.full_name}</a>!'
+        f'Goodbye, <a href="tg://user?id={member.id}">{member.full_name}</a>'
     )
 
 
