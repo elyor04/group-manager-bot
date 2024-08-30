7f1d67cfb567640d84b8cbf89c469ab49a47cdee
Elyor Tukhtamuratov <tuxtamuratovelyor@gmail.com>
2024-08-30 23:31:12 +0500

    update

--- handlers/write.py
+++ handlers/write.py
@@ -1,4 +1,5 @@
 from pyrogram import Client, filters, types
+from pyrogram.enums import ChatMembersFilter, ChatMemberStatus
 from pyrogram.handlers.message_handler import MessageHandler
 from utils.extractArgs import get_args
 
@@ -13,10 +14,18 @@ async def write_by_bot(client: Client, message: types.Message):
     chat_id = str(message.chat.id)
     args_text = get_args(message.text)
 
-    if (
-        (user_id not in allowed_users["all"])
-        and (user_id not in allowed_users.get(chat_id, []))
-    ) or (not args_text):
+    allowed_ids = [
+        admin.user.id
+        async for admin in message.chat.get_members(
+            filter=ChatMembersFilter.ADMINISTRATORS
+        )
+        if admin.status == ChatMemberStatus.OWNER
+    ]
+
+    allowed_ids.extend(allowed_users["all"])
+    allowed_ids.extend(allowed_users.get(chat_id, []))
+
+    if (user_id not in set(allowed_ids)) or (not args_text):
         return
 
     await message.delete()
