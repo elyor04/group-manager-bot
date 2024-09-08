2355d27e6af5af20caaaa90f7a80e5209c4fcebf
Elyor Tukhtamuratov <tuxtamuratovelyor@gmail.com>
2024-09-09 00:03:14 +0500

    turn off database logging

--- database/db.py
+++ database/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, Column, Integer
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("sqlite:///data/bot_data.db", echo=True)
+engine = create_engine("sqlite:///data/bot_data.db", echo=False)
 session = sessionmaker(bind=engine)()
 Base = declarative_base()
 
