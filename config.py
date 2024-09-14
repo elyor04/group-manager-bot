628603e31d7691928d517698b624287097da6f1f
Elyor Tukhtamuratov <91869056+elyor04@users.noreply.github.com>
2024-09-14 09:55:04 +0500

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Configuration settings
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
 API_ID = os.environ.get("API_ID")
 API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
