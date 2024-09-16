d01b721e2931e1e1d8233425b2b71082a731a193
Elyor Tukhtamuratov <91869056+elyor04@users.noreply.github.com>
2024-09-16 19:23:03 +0500

    remove validation

--- handlers/delete.py
+++ handlers/delete.py
@@ -4,9 +4,6 @@ from utils.chatMember import is_admin
 
 
 async def delete_message(message: types.Message):
-    if not await is_admin(message.chat, message.bot):
-        return
-
     if not await is_admin(message.chat, message.from_user):
         return
 
