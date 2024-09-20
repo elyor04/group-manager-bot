ae17e3a9c6fd1cfc21176c6f9571de1f84e5e9b5
Elyor Tukhtamuratov <tuxtamuratovelyor@gmail.com>
2024-09-20 11:22:13 +0500

    change data dir

--- app/config.py
+++ app/config.py
@@ -7,7 +7,7 @@ API_ID = os.environ.get("API_ID")
 API_HASH = os.environ.get("API_HASH")
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
-DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
+DATA_DIR = os.path.join(os.path.abspath("."), "data")
 DB_PATH = os.path.join(DATA_DIR, "bot_data.db")
 
 os.makedirs(DATA_DIR, exist_ok=True)
