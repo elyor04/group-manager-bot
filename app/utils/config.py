6643ff88af2f56debc141fff9dda322a9bd3996f
Elyor Tukhtamuratov <tuxtamuratovelyor@gmail.com>
2024-11-19 23:27:34 +0500

    changes

---
+++ app/utils/config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = os.environ.get("API_ID")
+API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+
+DATA_DIR = os.path.join(os.path.abspath("."), "data")
+DB_PATH = os.path.join(DATA_DIR, "sqlite.db")
+
+os.makedirs(DATA_DIR, exist_ok=True)
