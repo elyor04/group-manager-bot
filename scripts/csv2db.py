1a1d069d9225d90ec53ea0fd46f63c1ea2d85266
Elyor Tukhtamuratov <tuxtamuratovelyor@gmail.com>
2025-02-08 11:25:54 +0500

    update

--- scripts/csv2db.py
+++ scripts/csv2db.py
@@ -1,14 +1,40 @@
 import pandas as pd
-import sqlite3
+from tortoise import Tortoise, run_async
+from app.database import initialize_db, close_db
+from app.database.models import UserInfo
 
-csv_file = "data/sqlite.csv"
-db_file = "data/sqlite.db"
-table_name = "UserInfos"
 
-df = pd.read_csv(csv_file)
-conn = sqlite3.connect(db_file)
-df.to_sql(table_name, conn, if_exists="replace", index=False)
+async def init_db():
+    await initialize_db()
 
-conn.commit()
-conn.close()
-print(f"Data from {csv_file} has been written to {table_name} in {db_file}")
+    conn = Tortoise.get_connection("default")
+    await conn.execute_query("DROP TABLE IF EXISTS UserInfos")
+
+    await Tortoise.generate_schemas()
+
+
+async def import_data():
+    csv_file = "data/sqlite.csv"
+    df = pd.read_csv(csv_file)
+
+    await init_db()
+
+    users = [
+        UserInfo(
+            id=row["id"],
+            chat_id=row["chat_id"],
+            user_id=row["user_id"],
+            warnings=row["warnings"],
+            muted=row["muted"],
+            banned=row["banned"],
+            messages=row["messages"],
+        )
+        for _, row in df.iterrows()
+    ]
+    await UserInfo.bulk_create(users)
+
+    await close_db()
+    print(f"Data from {csv_file} has been written to the database.")
+
+
+run_async(import_data())
